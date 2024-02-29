7d9eaba66ea2c03d964556cc368a45b04f565088
Fantastik897 <sunnatbekyusupov18@gmail.com>
2024-02-29 10:41:07 +0500

    Update

--- main.js
+++ main.js
@@ -4,6 +4,8 @@
 
 //  5 sonini 2 ga bo'ling va qoldig`ini alertga chiqaring!
 
+// разделить 5 на 2 и вывести остаток в алерт (использовать оператор %)
+
 // javob:Pasdan yozib keting kodini
 
 
@@ -14,6 +16,8 @@
 
 // 1 dan 10 gacha bo'lgan random son yasang va alertga chiqaring
 
+// сделать рандомное число от 1 до 10 и вывести в алерт
+
 // Javob:Pasdan yozib keting kodini
 
 
@@ -22,6 +26,10 @@
 // 3-savol Math metodlari
 
 // 12.510 sonini butun songa aylantiring! 
+
+// сделать число 12.510 целым
+
+// ответ должен выйти 12
 
 // javob: 12 chiqishi kerak!
 
@@ -34,6 +42,8 @@
 
 // "MARS IT SCHOOL" sozini nechta harfdan iboratligini funksiya orqali topib va alertga chiqaring!.
 
+// с помощью функции найти сколько букв в слове "MARS IT SCHOOL" и вывести в алерт
+
 // Javob:Kodini yozib bering 
 
 
@@ -43,6 +53,8 @@
 // 5-savol for loop
 
 // "MARS IT SCHOOL" sozini javascript funksiyasi orqali 10 marta console.log ga chiqaring!
+
+// С помощью функции вывести в консоль 10 раз слово "MARS IT SCHOOL"
 
 // Javob:Kodini yozib bering
 
@@ -54,6 +66,8 @@
 
 // Shu arraydan foydalangan holda console.log ga o'zingizni ismigizni chiqaring
 
+// С помощью этого массива вывести в консоль свое имя
+
 // Javob:Kodini yozib bering
 
 
@@ -62,6 +76,8 @@
 // 7-savol if else
 
 // Promtdan yosh kiriting agar yoshi 18 yoshdan katta bo'lsa console da Siz balag'ot yoshiga yetgansiz , aks holda  siz balog'at yoshiga yetmagansiz  // agar yoshi 18 yoshga teng bolsa  balog'at yoshi muborak deb console da chiqarish kerak
+
+// Работа с if else, проверка возраста больше 18 лет, меньше 18 лет или равен 18 годам
 
 // Javob:Kodini yozib bering
 
@@ -72,6 +88,8 @@
 // 8-savol String metodlari vs Array metodlari
 
 // Promtdan ismigizni kiriting va console.log da ismigizni teskarisini chiqaring
+
+// Ввести в промпт свое имя и вывести в консоль его наоборот
 
 // Javob:Kodini yozib bering
 
@@ -86,6 +104,8 @@
 // masalan 4 raqamini kiritsam console.log da("Siz kiritgan raqam haftaning Payshanba kuniga to'gri keladi") bo'lib chiqish kerak
 // agar kiritilgan raqam 7 dan katta bolsa ya'ni 8 yoki 15 kiritilsa console da (Namalum raqam kiritdingiz ) deb chiqish kerak
 
+// ввести в промпт цифру от 1 до 7, и вывести к какому дню недели он равен, если больше 7 то сказать что такого дня нет
+
 
 // Javob:Kodini yozib bering
 
@@ -98,6 +118,8 @@
 // let sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // shu arraydagi juft sonlarni console ga chiqaring
+
+// найти и вывести четные массивы в консоль, с помощью методов
 
 
 // Javob :Kodini yozib bering
